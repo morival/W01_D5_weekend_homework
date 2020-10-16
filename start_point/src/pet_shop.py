5b0b4c41e9148c69d54065cc9e611d2c51f8e480
Mateusz <mateusz.harlos@gmail.com>
2020-10-16 21:01:28 +0100

    added False statement

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -61,3 +61,6 @@ def add_pet_to_customer(customer, new_pet):
 def customer_can_afford_pet(customer, new_pet):
     if customer["cash"] >= new_pet["price"]:
         return True
+    else:
+        return False
+
