a700be414c24d145ae58c322c28b901928506f82
Mateusz <mateusz.harlos@gmail.com>
2020-10-16 20:27:54 +0100

    added add pet to stock

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -41,3 +41,6 @@ def remove_pet_by_name(shop, pet_name):
     for pet in shop["pets"]:
         if pet["name"] == pet_name:
             shop["pets"].remove(pet)
+
+def add_pet_to_stock(shop, new_pet):
+    shop["pets"].append(new_pet)
