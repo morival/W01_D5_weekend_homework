15524bcb067fd96aad46def5f38014a3a9c8ceb2
Mateusz <mateusz.harlos@gmail.com>
2020-10-16 19:12:50 +0100

    added add_or_remove & pets_sold

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,8 +1,19 @@
 # WRITE YOUR FUNCTIONS HERE
+# import pdb
+
 def get_pet_shop_name(shop):
     return shop["name"]
 
 
-def get_total_cash(shop):
-    return shop ["admin"]["total_cash"]
+def get_total_cash(admin_cash):
+    return admin_cash["admin"]["total_cash"]
+
+
+def add_or_remove_cash(admin_cash, added_removed_cash):
+#     pdb.set_trace()
+    admin_cash["admin"]["total_cash"] += added_removed_cash
+    return admin_cash
+
+def get_pets_sold(pets_sold):
+    return pets_sold["admin"]["pets_sold"]
 
