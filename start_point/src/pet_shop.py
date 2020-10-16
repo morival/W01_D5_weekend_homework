d2732306a5de314081ed2bb4c11aff9e74229136
Mateusz <mateusz.harlos@gmail.com>
2020-10-16 20:37:25 +0100

    added remove customer cash

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -47,3 +47,7 @@ def add_pet_to_stock(shop, new_pet):
 
 def get_customer_cash(customer):
     return customer["cash"]
+
+def remove_customer_cash(customer, cash):
+    customer["cash"] -= cash
+    return customer
