a27671189fa5a7a463042b59d15e22c07cb56132
Mateusz <mateusz.harlos@gmail.com>
2020-10-16 20:41:15 +0100

    added count customer pet

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -51,3 +51,7 @@ def get_customer_cash(customer):
 def remove_customer_cash(customer, cash):
     customer["cash"] -= cash
     return customer
+
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
+
